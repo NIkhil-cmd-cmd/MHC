2817e39a862eba8c57f0b23544ca48079b66ed8a
Nikhil Krishnaswamy <84243022+NIkhil-cmd-cmd@users.noreply.github.com>
2023-09-12 21:41:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ with open('style.css') as f:
    
 st.title("Mental Health Chatbot")
 
-@st.experimental_memo def get_img_as_base64(file):
-   with open(file, "rb") as f:
-      data = f.read ()
-   return base64.b64encode (data) . decode ( )
-bg = get_img_as_base64 (" image. jpg")
+import base64
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-st.markdown(bg, unsafe_allow_html=True)
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 long_text = "Hello, this is the experiMENTAL health chatbot ui."
 
