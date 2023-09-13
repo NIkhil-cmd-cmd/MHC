86177f9a5e050e7fb5364fa62d1cf54f67b8e73c
Nikhil Krishnaswamy <84243022+NIkhil-cmd-cmd@users.noreply.github.com>
2023-09-12 19:24:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar = "U"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
