161fe96ddf9fee74f469c297d867c2cea92f4158
Nikhil Krishnaswamy <84243022+NIkhil-cmd-cmd@users.noreply.github.com>
2023-09-12 21:42:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('background.png')
+set_png_as_page_bg('magicpattern-grid-pattern-1694579841254.png')
 
 long_text = "Hello, this is the experiMENTAL health chatbot ui."
 
