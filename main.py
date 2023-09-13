aaf01ec4fe0682ba5a4432fda8f3f9b0c86f2f8c
Nikhil Krishnaswamy <84243022+NIkhil-cmd-cmd@users.noreply.github.com>
2023-09-12 21:50:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,30 @@ with open('style.css') as f:
    
 st.title("Mental Health Chatbot")
 
-import base64
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+ 
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "png"
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('magicpattern-grid-pattern-1694579841254.png')
+set_bg_hack('magicpattern-grid-pattern-1694579841254')
 
 long_text = "Hello, this is the experiMENTAL health chatbot ui."
 
